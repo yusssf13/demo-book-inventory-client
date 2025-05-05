5d37cf87acaa3e6dc5beded786905d7e2b2d36a8
Youssef Ben Lhaj <youssef@example.com>
2025-05-05 12:31:48 +0100

    firebase.config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,15 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCIQl0weHWkyX7FNmGNCTElUYL9aL0HPMA",
-    authDomain: "online-book-store-78516.firebaseapp.com",
-    projectId: "online-book-store-78516",
-    storageBucket: "online-book-store-78516.appspot.com",
-    messagingSenderId: "282181520275",
-    appId: "1:282181520275:web:0ce28538cea0433ea83666"
-  };
+  apiKey: "AIzaSyADbCpiUH_h4g-XJhb8NzI8txG6Tr9TafQ",
+  authDomain: "fir-book-inventory-98817.firebaseapp.com",
+  projectId: "fir-book-inventory-98817",
+  storageBucket: "fir-book-inventory-98817.firebasestorage.app",
+  messagingSenderId: "357062179477",
+  appId: "1:357062179477:web:f665a9838f2195f5e3799c"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export default app;
